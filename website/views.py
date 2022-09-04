d1c4ece07720fa1b5cb7d86899258415766b053f
jorgesoft <jorgecarrilloit@gmail.com>
2022-09-03 20:34:12 -0400

    added index url and view

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.base import TemplateView
+from django.views import View
 
 # Create your views here.
+
+
+class StartingPageView(TemplateView):
+    template_name = "website\index.html"
